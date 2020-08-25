195f63055dbad09114b6a7876686ede455f282ac
Pramothini Sekar <13pramo@gmail.com>
2020-08-26 00:57:23 +0530

    Home page v1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'; 
+import './Profile.scss';
+
+const Profile = () => {
+
+    return (
+        <div>
+            Profile page
+        </div>
+    );
+}
+
+export default Profile;
